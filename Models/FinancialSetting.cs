f063ea5ab58e2aff883cc55e07c2329885512928
blessedff <delovoyblessed@gmail.com>
2026-04-15 15:42:31 +0300

    Add FinancialSetting model

---
+++ Models/FinancialSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StationeryShop.Models
+{
+    [Table("FinancialSettings")]
+    public class FinancialSetting
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SettingId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string SettingKey { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
+        public string SettingValue { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string? Description { get; set; }
+    }
+}

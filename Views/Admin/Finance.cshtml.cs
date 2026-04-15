6a23be547ce6c7eb585f0a4f78c981a5439c0eea
blessedff <delovoyblessed@gmail.com>
2026-04-15 16:08:51 +0300

    Add Finance.cshtml

---
+++ Views/Admin/Finance.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StationeryShop.Views.Admin
+{
+    public class FinanceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
